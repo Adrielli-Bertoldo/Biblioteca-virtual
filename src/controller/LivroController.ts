3c30ceb47851f1a480e31e1d7aa0770821a3abee
Adrielli-Bertoldo <adrielli.bertoldo@gmail.com>
2025-06-13 10:52:21 -0300

    atualizar

--- src/controller/LivroController.ts
+++ src/controller/LivroController.ts
@@ -1,27 +1,87 @@
 import { Livro } from "../model/Livro";
 import { LivroRepository } from "../repository/LivroRespository";
 
-
 export class LivroController implements LivroRepository {
+
+    private listaLivros: Array<Livro> = new Array<Livro>();
+    private numero: number = 0;
+
+    /* Listar todos os livros */
     listarTodos(): Livro[] {
-        throw new Error("Method not implemented.");
-    }
-    listarPorId(id: number): Livro | undefined {
-        throw new Error("Method not implemented.");
-        
-    }
-    cadastrar(livro: Livro): void {
-        throw new Error("Method not implemented.");
-    }
-    atualizar(id: number, livro: Livro): boolean {
-        throw new Error("Method not implemented.");
-    }
-    deletar(id: number): boolean {
-        throw new Error("Method not implemented.");
-    }
-    buscarPorAno(ano: number): void {
-        throw new Error("Method not implemented.");
+        for (let livro of this.listaLivros) {
+            livro.visualizar();
+        }
+        return this.listaLivros;
     }
 
-  
+    /* Procurar livro por número */
+    procurarPorNumero(numero: number): void {
+        let buscaLivro = this.buscarNoArray(numero);
+
+        if (buscaLivro != null) {
+            buscaLivro.visualizar();
+        } else {
+            console.log("\nO livro número: " + numero + " não foi encontrado!");
+        }
+    }
+
+    /* Procurar livro por título */
+    procurarPorTitulo(titulo: string): void {
+        let encontrado = false;
+
+        for (let livro of this.listaLivros) {
+            if (livro.titulo.toLowerCase().includes(titulo.toLowerCase())) {
+                livro.visualizar();
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            console.log("\nNenhum livro com o título '" + titulo + "' foi encontrado.");
+        }
+    }
+
+    /* Atualizar livro */
+    atualizar(numero: number, livro: Livro): boolean {
+        let buscaLivro = this.buscarNoArray(numero);
+
+        if (buscaLivro != null) {
+            this.listaLivros[this.listaLivros.indexOf(buscaLivro)] = livro;
+            console.log("\nO Livro número: " + numero + " foi atualizado com sucesso!");
+            return true;
+        } else {
+            console.log("\nO Livro número: " + numero + " não foi encontrado!");
+            return false;
+        }
+    }
+
+    /* Deletar livro */
+    deletar(numero: number): boolean {
+        let buscaLivro = this.buscarNoArray(numero);
+
+        if (buscaLivro != null) {
+            this.listaLivros.splice(this.listaLivros.indexOf(buscaLivro), 1);
+            console.log("\nO Livro número: " + numero + " foi deletado com sucesso!");
+            return true;
+        } else {
+            console.log("\nO Livro número: " + numero + " não foi encontrado!");
+            return false;
+        }
+    }
+
+    /* Gerar número do livro */
+    public gerarNumero(): number {
+        return ++this.numero;
+    }
+
+    /* Buscar livro no array por número */
+    private buscarNoArray(numero: number): Livro | null {
+        for (let livro of this.listaLivros) {
+            if (livro.numero === numero)
+                return livro;
+        }
+        return null;
+    }
 }
+
+
